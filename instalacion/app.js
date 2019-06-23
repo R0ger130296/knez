d7e4ade60c46cbeb558682939f77bdb7503a2e6a
Omar Cedeño <grarogccee@gmail.com>
2019-06-23 09:49:35 -0500

    cambioEnRuta

--- instalacion/app.js
+++ instalacion/app.js
@@ -3,7 +3,7 @@ const express = require('express'),
     bodyParser = require('body-parser')
 
 let app = express(),
-  rutas = require('../rutas/persona')
+  rutas = require('../rutas/ruta')
 
 
 app.use(bodyParser.urlencoded({extended:false}))
