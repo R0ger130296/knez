77a52915bb9277249b0bb7d32039327e9e8b4e01
Omar Cedeño <grarogccee@gmail.com>
2019-06-27 19:47:46 -0500

    metodoEliminar

--- controles/Kawina.js
+++ controles/Kawina.js
@@ -3,6 +3,25 @@ let config = require('../knexfile')
 let env = 'development'
 let db = require('knex')(config[env])
 
+let getDatos = (req, res) =>{
+    let tabla = req.query.tabla
+    let campo = req.query.campo
+    db.select(campo).from(tabla)
+    .then(response =>{
+        return res.status(200).json({
+            ok: true,
+            data: response,
+            mensaje: `Existen ${response.length} registros`
+        })
+    })
+    .catch(error =>{
+        return res.status(500).json({
+            ok: false,
+            data: null,
+            mensaje: `error: ${error}`
+        })
+    })
+}
 let postDatos = (req, res) =>{
     let tabla = req.body.tabla
     let registro = req.body.registros
@@ -22,28 +41,51 @@ let postDatos = (req, res) =>{
         })
     })
 }
-
-let getDatos = (req, res) =>{
-    let tabla = req.query.tabla
-    let campo = req.query.campo
-    db.select(campo).from(tabla)
-    .then(response =>{
+let deleteDatos = (req,res) =>{
+    let tabla= req.query.tabla
+    let id = req.query.id
+    db(tabla).where('id',id).del()
+    .then(data => {
         return res.status(200).json({
             ok: true,
-            data: response,
-            mensaje: `total de datos`
+            datos: data,
+            mensaje: `se elimino el dato ${id}`
         })
     })
-    .catch(error =>{
+    .catch((error) => {
         return res.status(500).json({
             ok: false,
-            data: null,
-            mensaje: `error: ${error}`
+            datos: null,
+            mensaje: `Error del servidor: ${error}`//solo en desarollo esta linea no es produccion
+        })
+    })
+}
+let updateDatos =(req,res)=>{
+    let tabla = req.body.tabla
+    let datos = req.body.registros
+    let datas ="";
+    datos.forEach(elementos=>{
+        datas=elementos;
+    });
+    db(tabla).where('id',datas.id).update(datas)
+    .then(response=>{
+        return res.status(200).json({
+            ok:true,
+            data:response,
+        })
+    })
+    .catch(error=>{
+        return res.estatus(500).json({
+            of:true,
+            data:error,
+             mensaje: `Error del servidor: ${error}`//solo en desarollo esta linea no es produccion
         })
     })
 }
 
 module.exports = {
     postDatos,
-    getDatos
+    getDatos,
+    deleteDatos,
+    updateDatos,
 }
--- rutas/ruta.js
+++ rutas/ruta.js
@@ -3,8 +3,9 @@ const express = require('express')
 let api = express.Router(),
   control = require('../controles/Kawina')
 
-api.get('/Kawina', control.getDatos)
-api.post('/Kawina', control.postDatos)
-
+api.get('/Kawina/leer', control.getDatos)
+api.post('/Kawina/insertar', control.postDatos)
+api.delete('/Kawina/borrar',control.deleteDatos)
+api.put('Kawina/actualizar',control.updateDatos)
 
 module.exports = api
